690d5e62e94e01a93fc8bd42353fd3edabe96ff7
Alysson Eduardo <alysson6801@hotmail.com>
2022-09-18 11:32:16 -0300

    chore: add Main app component to site Vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
-  <h1 class="mt-8 text-4xl text-center">Hello World</h1>
+  <MainNav />
 </template>
 
 <script>
+import MainNav from "@/components/MainNav.vue";
+
 export default {
   name: "App",
+  components: {
+    MainNav,
+  },
 };
 </script>
